4d08fca051d4b66bce4cde141fdc4caba3208d56
wawancoding18 <dzakwan112016@gmail.com>
2023-12-13 15:51:46 +0700

    first commit

---
+++ game batu gunting kertas.js
@@ -0,0 +1,52 @@
+// var quest = true;
+
+// while(quest){
+// //prompt
+// var player = prompt("Pilihlah salah satu dari gunting, batu, dan kertas")
+
+// //computer
+// var computer = Math.random()
+
+// if (computer < 0.34){
+//     computer= 'gunting'
+// }
+
+// else if(computer >= 0.34 && computer< 0.67){
+//     computer= 'batu'
+// }
+
+// else{
+//     computer= 'kertas'
+// }
+
+
+// // statement
+// var hasil = ''
+// if (player == computer){
+//      hasil= 'seri'
+
+// } else if (player == 'gunting'){
+//        hasil = (computer == 'batu') ? 'LOSE!' : 'WIN!';
+
+// }else if (player == 'batu'){
+//     hasil = (computer == 'gunting') ? 'WIN!' : 'LOSE!';
+
+
+// }else if (player == 'kertas'){
+//     hasil = (computer == 'batu') ? 'WIN!' : 'LOSE!';
+// } else {
+//     hasil = 'Memasukkan pilihan yang salah!'
+// }
+
+// //tampilan hasilnya
+// alert('Kamu memilih: '+player+' dan Compute memilih: '+computer+'\n Maka hasilnya: ' + hasil)
+
+// quest = confirm("Again?")
+
+
+// }
+
+// alert("Thanks dah mainin gamenya")
+
+ 
+
